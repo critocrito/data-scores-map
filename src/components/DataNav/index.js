5f843a58de14231a654dc939118b9386b94c4d62
Christo <christo@cryptodrunks.net>
2018-05-04 23:16:16 +0200

    feat(client): Toggle between keyword and city listings.

--- src/components/DataNav/index.js
+++ src/components/DataNav/index.js
@@ -11,6 +11,8 @@ type State = {
   citiesAll: Array<City>,
   citiesCount: number,
   selectedKeywords: Array<string>,
+  selectedCities: Array<string>,
+  list: string,
 };
 
 class DataNav extends React.Component<{}, State> {
@@ -19,6 +21,8 @@ class DataNav extends React.Component<{}, State> {
     cities: [],
     citiesAll: [],
     selectedKeywords: [],
+    selectedCities: [],
+    list: "keywords",
   };
 
   async componentDidMount() {
@@ -32,45 +36,80 @@ class DataNav extends React.Component<{}, State> {
     );
     // eslint-disable-next-line react/no-did-mount-set-state
     this.setState({
-      cities,
       citiesAll: cities,
       citiesCount: cities.length,
+      cities,
     });
+  }
+
+  isSelectedCity(id: string): boolean {
+    return this.state.selectedCities.includes(id);
   }
 
   isSelectedKeyword(keyword: string): boolean {
     return this.state.selectedKeywords.includes(keyword);
   }
 
+  toggleSelectedCity(id: string) {
+    const {selectedCities, citiesAll} = this.state;
+    const selected = this.isSelectedCity(id)
+      ? selectedCities.filter(i => i !== id)
+      : selectedCities.concat(id);
+    const cities =
+      selected.length > 0
+        ? citiesAll.filter(city => selected.includes(city.id))
+        : citiesAll;
+    this.setState({
+      selectedCities: selected,
+      citiesCount: selected.length,
+      cities,
+    });
+  }
+
   toggleSelectedKeyword(keyword: string) {
     const {selectedKeywords, citiesAll} = this.state;
-    const newSelected = this.isSelectedKeyword(keyword)
+    const selected = this.isSelectedKeyword(keyword)
       ? selectedKeywords.filter(k => k !== keyword)
       : selectedKeywords.concat(keyword);
     const cities =
-      newSelected.length > 0
+      selected.length > 0
         ? citiesAll.filter(city =>
             city.keywords.reduce((memo, key) => {
               if (memo) return memo;
-              return newSelected.includes(key);
+              return selected.includes(key);
             }, false),
           )
         : citiesAll;
     this.setState({
-      selectedKeywords: newSelected,
+      selectedKeywords: selected,
       citiesCount: cities.length,
       cities,
     });
   }
 
+  toggleList() {
+    const {list, citiesAll} = this.state;
+    const newList = list === "keywords" ? "cities" : "keywords";
+    this.setState({
+      cities: citiesAll,
+      list: newList,
+      selectedKeywords: [],
+      selectedCities: [],
+    });
+  }
+
   render() {
-    const {citiesCount, cities, citiesAll, selectedKeywords} = this.state;
+    const {citiesCount, cities, citiesAll, list, selectedKeywords} = this.state;
     return (
       <article className="flex">
         <div className="sp-wrapper w-third">
           <SidePanel
             isSelectedKeyword={k => this.isSelectedKeyword(k)}
             selectKeywordHandler={k => this.toggleSelectedKeyword(k)}
+            isSelectedCity={c => this.isSelectedCity(c)}
+            selectCityHandler={c => this.toggleSelectedCity(c)}
+            toggleList={() => this.toggleList()}
+            activeList={list}
             cities={cities}
             citiesAll={citiesAll}
           />
--- src/components/SidePanel/index.js
+++ src/components/SidePanel/index.js
@@ -10,8 +10,12 @@ import type {City} from "../../lib/types";
 
 type Props = {
   citiesAll: Array<City>,
-  selectKeywordHandler: string => void,
+  activeList: string,
   isSelectedKeyword: string => boolean,
+  isSelectedCity: string => boolean,
+  selectKeywordHandler: string => void,
+  selectCityHandler: string => void,
+  toggleList: void => void,
 };
 
 const [color, colorAlt] = ["rgba(0, 108, 183, 0.7)", "rgba(0, 108, 183, 0.4)"];
@@ -43,11 +47,32 @@ const chartOptions = {
   categoryPercentage: 0.7,
 };
 
+const rowItem = (
+  key: string,
+  classNames: string,
+  clickHandler: () => void,
+  chartData: {[string]: any},
+  content: React.Node,
+) => (
+  <li key={key} className={classNames}>
+    <button
+      className="link pa0 ma0 w-100"
+      onClick={clickHandler}
+      onKeyPress={clickHandler}
+    >
+      <div className="flex pa1 items-center">
+        <div className="w-50 tl">{content}</div>
+        <div className="sp-chart w-40">
+          <HorizontalBar data={chartData} options={chartOptions} />
+        </div>
+      </div>
+    </button>
+  </li>
+);
+
 class SidePanel extends React.Component<Props> {
   static defaultProps = {
     citiesAll: [],
-    selectKeywordHandler: () => null,
-    isSelectedKeyword: () => false,
   };
 
   keywordsList() {
@@ -93,36 +118,89 @@ class SidePanel extends React.Component<Props> {
           "sp-row--active": this.props.isSelectedKeyword(k),
         });
         const clickHandler = () => this.props.selectKeywordHandler(k);
-
-        return (
-          <li key={k} className={classNames}>
-            <button
-              className="link pa0 ma0 w-100"
-              onClick={clickHandler}
-              onKeyPress={clickHandler}
-            >
-              <div className="flex pa1 items-center">
-                <div className="b ttu w-30">{k}</div>
-                <div className="sp-chart w-40">
-                  <HorizontalBar data={chartData} options={chartOptions} />
-                </div>
-              </div>
-            </button>
-          </li>
-        );
+        const content = <div className="b tty">{k}</div>;
+        return rowItem(k, classNames, clickHandler, chartData, content);
       });
   }
+
+  citiesList() {
+    const cityStats = this.props.citiesAll.reduce((memo, city) => {
+      const unitsCount = Object.keys(city.unitsByKeywords).reduce(
+        (acc, key) => acc + city.unitsByKeywords[key].length,
+        0,
+      );
+
+      Object.assign(memo, {
+        [city.id]: {
+          city,
+          unitsCount,
+          keywordsCount: city.keywords.length,
+        },
+      });
+
+      return memo;
+    }, {});
+
+    return Object.keys(cityStats)
+      .sort((a, b) => a.localeCompare(b))
+      .map(id => {
+        const {city, unitsCount, keywordsCount} = cityStats[id];
+
+        const chartData = {
+          labels: ["units", "keywords"],
+          datasets: [
+            {
+              backgroundColor: [color, colorAlt],
+              borderWidth: 0,
+              hoverBackgroundColor: [color, colorAlt],
+              data: [unitsCount, keywordsCount],
+            },
+          ],
+        };
+        const classNames = classnames({
+          "sp-row": true,
+          "w-100": true,
+          "sp-row--active": this.props.isSelectedCity(id),
+        });
+        const clickHandler = () => this.props.selectCityHandler(id);
+        const content = (
+          <div>
+            <span className="b ttu">{city.name}</span>
+            <br />
+            <span className="f7 i">{city.county}</span>
+          </div>
+        );
+        return rowItem(id, classNames, clickHandler, chartData, content);
+      });
+  }
+
   render() {
-    const children = this.keywordsList();
+    const {activeList, toggleList} = this.props;
+    const clickHandler = () => toggleList();
+    const children =
+      activeList === "keywords" ? this.keywordsList() : this.citiesList();
+    const keywordsToggle =
+      activeList === "keywords" ? (
+        <button>Keywords</button>
+      ) : (
+        <button onClick={clickHandler} onKeyPress={clickHandler}>
+          Keywords
+        </button>
+      );
+    const citiesToggle =
+      activeList === "cities" ? (
+        <button>Cities</button>
+      ) : (
+        <button onClick={clickHandler} onKeyPress={clickHandler}>
+          Cities
+        </button>
+      );
+
     return (
       <article className="sp pa0 ma0">
         <section className="sp-header flex pa0 ma0">
-          <div className="w-50">
-            <button>Keywords</button>
-          </div>
-          <div className="w-50">
-            <button>Cities</button>
-          </div>
+          <div className="w-50">{keywordsToggle}</div>
+          <div className="w-50">{citiesToggle}</div>
         </section>
         <section className="flex pa0 ma0 w-100">
           <ul className="list pa0 ma0 w-100">{children}</ul>
