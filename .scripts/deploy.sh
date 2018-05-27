6c5c54d99f22cd54ecffdd459c46107bed589ccd
Christo <christo@cryptodrunks.net>
2018-05-27 20:23:01 +0200

    chore: Print time during deploy.

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-NOW=$(date +%Y-%m-%d)
+NOW=$(date +%Y-%m-%d_%H:%M)
 
 echo "Deploying at $NOW"
 scp -i data-scores -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rp dist/* "$DEPLOY_SSH_USER"@"$DEPLOY_SSH_HOST":"$DEPLOY_SSH_PATH"
