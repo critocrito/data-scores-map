c6b6764f2f0f159b1002bc45206319078f4861ed
Christo <christo@cryptodrunks.net>
2018-05-27 20:09:01 +0200

    chore: Print deploy time.

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 set -e
 
+NOW=$(date +%Y-%m-%d)
+
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "$DEPLOY_BRANCH" ]; then
+  echo "Deploying at $NOW"
   scp -i data-scores -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rp dist/* "$DEPLOY_SSH_USER"@"$DEPLOY_SSH_HOST":"$DEPLOY_SSH_PATH"
 fi
