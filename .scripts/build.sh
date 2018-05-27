3a39f7e1144b281297b5f5deba1101cbee06d200
Christo <christo@cryptodrunks.net>
2018-05-27 20:02:02 +0200

    fix: Changed the deploy command.

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-set -e # halt script on error
+set -e
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "$DEPLOY_BRANCH" ]; then
   echo "Building site"
--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-set -e # halt script on error
+set -e
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "$DEPLOY_BRANCH" ]; then
-  scp -i data-scores -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rp dist "$SSH_USER"@"$SSH_HOST":"$SSH_PATH"
+  scp -i data-scores -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -rp dist/* "$DEPLOY_SSH_USER"@"$DEPLOY_SSH_HOST":"$DEPLOY_SSH_PATH"
 fi
