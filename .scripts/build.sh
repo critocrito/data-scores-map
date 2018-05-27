eaafc221991a2407e176f9db904450a2e661dc6b
Christo <christo@cryptodrunks.net>
2018-05-27 20:16:59 +0200

    refactor: run build and deploy scripts in any case.

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ "$TRAVIS_BRANCH" = "$DEPLOY_BRANCH" ]; then
-  echo "Building site"
-  npm run lint
-  npm run compile
-fi
+echo "Building site"
+npm run lint
+npm run compile
