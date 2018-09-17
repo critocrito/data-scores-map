f41ee6a9e4fa28eda5d611d5800cec1a046c85f0
Christo <christo@cryptodrunks.net>
2018-09-17 15:15:47 +0200

    refactor(client): extracted pagination into a HOC

--- src/components/DocumentsTable/index.js
+++ src/components/DocumentsTable/index.js
@@ -2,120 +2,63 @@
 import * as React from "react";
 
 import DocumentsTableRow from "../DocumentsTableRow";
+import pageableList from "../pageableList";
 import type {Document} from "../../lib/types";
 
 type Props = {
   documents: Document[],
-  documentsTotal: number,
-  paginateDocuments: (page: number) => void,
-  pageSize: number,
+  total: number,
+  pagination: React.Node[],
 };
 
-type State = {
-  page: number,
-};
+const DocumentsTable = ({documents, total, pagination}: Props) =>
+  documents.length > 0 ? (
+    <table className="collapse w-100">
+      <thead>
+        <tr>
+          <td className="pv2 ph3 tc fw6 ttu bb">Document ({total})</td>
+          <td className="pv2 ph3 tc fw6 ttu bb">Authorities</td>
+          <td className="pv2 ph3 tc fw6 ttu bb">Departments</td>
+          <td className="pv2 ph3 tc fw6 ttu bb">Companies</td>
+          <td className="pv2 ph3 tc fw6 ttu bb">Systems</td>
+          <td className="pv2 ph3 tc fw6 ttu bb">Source</td>
+        </tr>
+      </thead>
+      <tbody>
+        {documents.map(
+          ({
+            id,
+            title,
+            authorities,
+            departments,
+            source,
+            companies,
+            systems,
+          }) => (
+            <DocumentsTableRow
+              key={id}
+              id={id}
+              title={title}
+              authorities={authorities}
+              departments={departments}
+              source={source}
+              companies={companies}
+              systems={systems}
+            />
+          ),
+        )}
+      </tbody>
 
-class DocumentsTable extends React.Component<Props, State> {
-  state = {
-    page: 0,
-  };
+      <tfoot>
+        <tr>
+          <td className="pv2 ph3 tc" colSpan={6}>
+            {pagination}
+          </td>
+        </tr>
+      </tfoot>
+    </table>
+  ) : (
+    ""
+  );
 
-  paginate = async (page: number) => {
-    const {paginateDocuments, pageSize} = this.props;
-    paginateDocuments(page * pageSize);
-    this.setState({page});
-  };
-
-  pagination(): Array<React.Node> {
-    const {documentsTotal, pageSize} = this.props;
-    const {page} = this.state;
-    const pages = Math.ceil(documentsTotal / pageSize);
-    const left =
-      page === 0 ? (
-        <span key="left" />
-      ) : (
-        <span
-          className="pointer"
-          key="left"
-          onClick={() => this.paginate(page - 1)}
-          onKeyPress={() => this.paginate(page - 1)}
-          role="button"
-          tabIndex={0}
-        >
-          &#60; Previous&nbsp;&nbsp;&nbsp;&nbsp;
-        </span>
-      );
-    const right =
-      page < pages - 1 ? (
-        <span
-          className="pointer"
-          key="right"
-          onClick={() => this.paginate(page + 1)}
-          onKeyPress={() => this.paginate(page + 1)}
-          role="button"
-          tabIndex={0}
-        >
-          &nbsp;&nbsp;&nbsp;&nbsp;Next &#62;
-        </span>
-      ) : (
-        <span key="right" />
-      );
-    return [left, right];
-  }
-
-  render() {
-    const {documents, documentsTotal} = this.props;
-    return documents.length > 0 ? (
-      <table className="collapse w-100">
-        <thead>
-          <tr>
-            <td className="pv2 ph3 tc fw6 ttu bb">
-              Document ({documentsTotal})
-            </td>
-            <td className="pv2 ph3 tc fw6 ttu bb">Authorities</td>
-            <td className="pv2 ph3 tc fw6 ttu bb">Departments</td>
-            <td className="pv2 ph3 tc fw6 ttu bb">Companies</td>
-            <td className="pv2 ph3 tc fw6 ttu bb">Systems</td>
-            <td className="pv2 ph3 tc fw6 ttu bb">Source</td>
-          </tr>
-        </thead>
-        <tbody>
-          {documents.map(
-            ({
-              id,
-              title,
-              authorities,
-              departments,
-              source,
-              companies,
-              systems,
-            }) => (
-              <DocumentsTableRow
-                key={id}
-                id={id}
-                title={title}
-                authorities={authorities}
-                departments={departments}
-                source={source}
-                companies={companies}
-                systems={systems}
-              />
-            ),
-          )}
-        </tbody>
-
-        <tfoot>
-          <tr>
-            <td className="pv2 ph3 tc" colSpan={6}>
-              {this.pagination()}
-            </td>
-          </tr>
-        </tfoot>
-      </table>
-    ) : (
-      ""
-    );
-  }
-}
-
-export default DocumentsTable;
+export default pageableList(DocumentsTable);
---
+++ src/components/pageableList/index.js
@@ -0,0 +1,88 @@
+// @flow
+import * as React from "react";
+
+import type {Document} from "../../lib/types";
+
+type Props = {
+  documents: Document[],
+  documentsTotal: number,
+  paginateDocuments: (page: number) => void,
+  pageSize: number,
+};
+
+type State = {
+  page: number,
+};
+
+const pageableList = (
+  WrappedComponent: React.ComponentType<{
+    documents: Document[],
+    total: number,
+    pagination: React.Node[],
+  }>,
+) =>
+  class extends React.Component<Props, State> {
+    static displayName = "Hahaha";
+
+    state = {
+      page: 0,
+    };
+
+    paginate = async (page: number) => {
+      const {paginateDocuments, pageSize} = this.props;
+      paginateDocuments(page * pageSize);
+      this.setState({page});
+    };
+
+    pagination(): Array<React.Node> {
+      const {documentsTotal, pageSize} = this.props;
+      const {page} = this.state;
+      const pages = Math.ceil(documentsTotal / pageSize);
+      const left =
+        page === 0 ? (
+          <span key="left" />
+        ) : (
+          <span
+            className="pointer"
+            key="left"
+            onClick={() => this.paginate(page - 1)}
+            onKeyPress={() => this.paginate(page - 1)}
+            role="button"
+            tabIndex={0}
+          >
+            &#60; Previous&nbsp;&nbsp;&nbsp;&nbsp;
+          </span>
+        );
+      const right =
+        page < pages - 1 ? (
+          <span
+            className="pointer"
+            key="right"
+            onClick={() => this.paginate(page + 1)}
+            onKeyPress={() => this.paginate(page + 1)}
+            role="button"
+            tabIndex={0}
+          >
+            &nbsp;&nbsp;&nbsp;&nbsp;Next &#62;
+          </span>
+        ) : (
+          <span key="right" />
+        );
+      return [left, right];
+    }
+
+    render() {
+      const {documents, documentsTotal} = this.props;
+      return documents.length > 0 ? (
+        <WrappedComponent
+          documents={documents}
+          total={documentsTotal}
+          pagination={this.pagination()}
+        />
+      ) : (
+        ""
+      );
+    }
+  };
+
+export default pageableList;
