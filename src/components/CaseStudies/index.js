b092d7949a3e0b1d50fc1cdfa37ac4c7efcbbb40
olisol <9976726+olisol@users.noreply.github.com>
2018-11-12 14:25:26 +0100

    Update index.js
    Update case studies

--- src/components/CaseStudies/index.js
+++ src/components/CaseStudies/index.js
@@ -25,25 +25,15 @@ const CaseStudies = () => (
     </article>
     <div className="vh">
     <div className="w-80-ns mv4 pa2 ba b--light-silver shadow-4 center">
-      <h2 className="pa3 f3 primary-color">
-        Integrated Analytical Hub – Bristol
-      </h2>
-      <p className="pa3">
-        The Integrated Analytical Hub created internally by Bristol City Council
-        started its development in 2016 in the context of the Troubled Families
-        programme that launched in 2011. The Hub was initially built as a ‘data
-        warehouse’ around the Troubled Families programme to provide Government
-        with auditing data and to link data on people living in Bristol City
-        that meet the programme’s criteria. This data includes 30 different
-        social issue datasets, including crime records, unemployment, school
-        attendance, and mental health provision. In its current phase, the data
-        warehouse is also being used for analytics and risk profiling, or what
-        is described by the Council as ‘targeted interventions’ and ‘targeted
-        risk assessments’, currently focused on child exploitation. Profiling
-        confirmed victims in recent years produce a statistics-based risk
-        assessment. This includes a score between 0 and 100 for every young
-        person in the database that is provided to the relevant case worker.
-      </p>
+<div class="w-80-ns mv4 pa2 ba b--light-silver shadow-4 center h-inherit">
+ <div class="fl w-100 w-30-ns tc">
+    <h1>IMG</h1>
+  </div>
+  <div class="fl w-100 w-70-ns tl">
+  <h2 class="pa3 f3 primary-color">Integrated Analytical Hub – Bristol</h2><p class="pa3">The Integrated Analytical Hub created internally by Bristol City Council started its development in 2016 in the context of the Troubled Families programme that launched in 2011. The Hub was initially built as a ‘data warehouse’ around the Troubled Families programme to provide Government with auditing data and to link data on people living in Bristol City that meet the programme’s criteria. This data includes 30 different social issue datasets, including crime records, unemployment, school attendance, and mental health provision. In its current phase, the data warehouse is also being used for analytics and risk profiling, or what is described by the Council as ‘targeted interventions’ and ‘targeted risk assessments’, currently focused on child exploitation. Profiling confirmed victims in recent years produce a statistics-based risk assessment. This includes a score between 0 and 100 for every young person in the database that is provided to the relevant case worker.</p>
+ </div>
+</div>
+
     </div></div>
     <Footer />
   </div>
