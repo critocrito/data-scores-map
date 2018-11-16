1307747a351086f7df937dd1fb53f5dfe9a8d5d6
Christo <christo@cryptodrunks.net>
2018-11-16 20:03:10 +0100

    fix(client): fixed the layout of the methodology page

--- src/components/Methodology/index.js
+++ src/components/Methodology/index.js
@@ -1,7 +1,6 @@
 // @flow
 import * as React from "react";
 
-// import "./index.css";
 import Header from "../Header";
 import Footer from "../Footer";
 
@@ -30,51 +29,58 @@ const Methodology = () => (
         <img className="w-40 pl7 pb2" alt="" src="/images/case-studies.png" />
       </div>
     </article>
-    <div className="w-90">
-      <p>
-        Drawing from the Algorithm Tips’{" "}
-        <a
-          className="link primary-color"
-          href="http://algorithmtips.org/methodology/"
-        >
-          methodology
-        </a>
-        , we used search engines to scrape UK government websites - those ending
-        in “gov.uk”, “nhs.uk”, “police.uk”, “mod.uk” and “sch.uk” - for
-        information and documents relating to data systems. We adapted the
-        keywords used by Algorithm Tips as keywords in the search, adding our
-        own terms that we thought may be more likely to return productive
-        results in a UK context. We also included the names of software and
-        companies encountered during initial desk research. The final list of
-        keywords includes XX terms. A different and shorter list of keywords
-        using more generic terminology was used to add scrapes from newspaper
-        websites, limited to the largest national and local newspapers, to the
-        database. Finally, we added data from a list of over 400 Freedom of
-        Information requests that we had sent out.
-      </p>
+    <div className="vh-100 pa3 ma3 w-80-ns db relative center">
+      <div className="w-80">
+        <p>
+          Drawing from the Algorithm Tips’{" "}
+          <a
+            className="link primary-color"
+            href="http://algorithmtips.org/methodology/"
+          >
+            methodology
+          </a>
+          , we used search engines to scrape UK government websites - those
+          ending in “gov.uk”, “nhs.uk”, “police.uk”, “mod.uk” and “sch.uk” - for
+          information and documents relating to data systems. We adapted the
+          keywords used by Algorithm Tips as keywords in the search, adding our
+          own terms that we thought may be more likely to return productive
+          results in a UK context. We also included the names of software and
+          companies encountered during initial desk research. The final list of
+          keywords includes XX terms. A different and shorter list of keywords
+          using more generic terminology was used to add scrapes from newspaper
+          websites, limited to the largest national and local newspapers, to the
+          database. Finally, we added data from a list of over 400 Freedom of
+          Information requests that we had sent out.
+        </p>
 
-      <p>
-        The results of these searches and data-sets have been used to populate a
-        database. This database includes all unverified data from those
-        searches. For the Data Insights section, keyword matches have been
-        paired with names of local authorities and council, organisations, and
-        mentions of government departments categorised according to the
-        government’s own typology, in order to provide visualisations of the
-        data.
-      </p>
+        <p>
+          The results of these searches and data-sets have been used to populate
+          a database. This database includes all unverified data from those
+          searches. For the Data Insights section, keyword matches have been
+          paired with names of local authorities and council, organisations, and
+          mentions of government departments categorised according to the
+          government’s own typology, in order to provide visualisations of the
+          data.
+        </p>
 
-      <p>
-        Finally, the tool includes summaries of 6 case studies that were
-        researched for the project, selected for geographical and topical
-        spread, supplementing desk research with interviews for an in-depth
-        qualitative analysis of developments.
-      </p>
+        <p>
+          Finally, the tool includes summaries of 6 case studies that were
+          researched for the project, selected for geographical and topical
+          spread, supplementing desk research with interviews for an in-depth
+          qualitative analysis of developments.
+        </p>
 
-      <p>
-        Follow this link for a code repository which contains the resources
-        required to replicate this research:
-        https://github.com/critocrito/data-scores-in-the-uk
-      </p>
+        <p>
+          Follow this link for a code repository which contains the resources
+          required to replicate this research:{" "}
+          <a
+            className="link primary-color"
+            href="https://github.com/critocrito/data-scores-in-the-uk"
+          >
+            https://github.com/critocrito/data-scores-in-the-uk
+          </a>
+        </p>
+      </div>
     </div>
     <Footer />
   </div>
