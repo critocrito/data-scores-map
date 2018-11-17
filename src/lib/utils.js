2302f588327a818aa89e8ee3aeaa725749e81911
Christo <christo@cryptodrunks.net>
2018-11-17 10:36:41 +0100

    refactor: adapted to new source scheme

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,14 +9,12 @@ export const sourcify = (source: string): string => {
   switch (source) {
     case "government website":
       return ".gov.uk";
-    case "auxiliary website":
-      return ".gov.uk";
     case "media website":
       return "Media";
     case "foi":
       return "FOI request";
     default:
-      return "unknown";
+      return source;
   }
 };
 
