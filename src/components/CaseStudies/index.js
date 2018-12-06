607c18548425fe4d80cc91362481988790d048d5
Christo <christo@cryptodrunks.net>
2018-12-06 10:07:06 +0100

    feat: added all case studies

--- src/components/CaseStudies/index.js
+++ src/components/CaseStudies/index.js
@@ -23,15 +23,15 @@ const CaseStudies = () => (
         <img className="w-40 pl7 pb2" alt="" src="/images/case-studies.png" />
       </div>
     </article>
-    <div className="vh-100 pa3 ma3 w-100-ns db relative center">
-      <div className="w-80 center ">
-        <div className="fl w-30 tl ">
+    <div className="pa3 ma3 w-100-ns db relative center">
+      <div className="w-80 center flex">
+        <div className="fl w-30 mt3 mb3">
           <img
             alt="Bristol Integrated Analytical Hub"
-            src="images/case_bristol.png"
+            src="images/case_bristol.jpg"
           />
         </div>
-        <div className="fl w-70 tl pb4">
+        <div className="fl w-70 tl pb4 ml3">
           <h2 className="pa3 f3 primary-color">
             Bristol Integrated Analytical Hub
           </h2>
@@ -61,47 +61,50 @@ const CaseStudies = () => (
         </div>
       </div>
 
-      <div className="w-80 center  ">
-        <div className="fl w-30 tl ">
-          <img
-            alt="Bristol Integrated Analytical Hub"
-            src="images/case_camdem.png"
-          />
+      <div className="w-80 center flex">
+        <div className="fl w-30 mt3">
+          <img alt="Camden Resident Index" src="images/case_camdem.jpg" />
         </div>
-        <div className="fl w-70 tl pb4">
+        <div className="fl w-70 tl pb4 ml3">
           <h2 className="pa3 f3 primary-color">Camden Resident Index</h2>
           <p className="pa3 f5">
             The Camden Resident Index is a data management system utilising
             software supplied by IBM that allows for a ‘single view of a
             citizen’ by aggregating data from 16 different council business
             systems across Camden Council, covering 123 fields of primarily
-            demographic information. It is to date the largest data management
-            installation in local government in the UK. It was created in 2013
-            following the closure of the national children’s database Contact
-            Point to uphold multi-agency work. It uses probabilistic matching
-            technology to match individuals or households across the different
-            business systems, in which records are matched together to produce a
+            demographic information. It is to date the{" "}
+            <a
+              href="https://www.involve.org.uk/sites/default/files/field/attachemnt/3-case-studies-data-sharing.pdf"
+              className="link primary-color"
+            >
+              largest data management installation
+            </a>{" "}
+            in local government in the UK. It was created in 2013 following the
+            closure of the national children’s database Contact Point to uphold
+            multi-agency work. It uses probabilistic matching technology to
+            match individuals or households across the different business
+            systems, in which records are matched together to produce a
             comparison score that indicates the likelihood of records belonging
             to the same person or family. The Camden Resident Index is used by
             the Multi-Agency Safeguarding Hub to locate information about a
             household’s engagement with services across the Council. A key use
-            of the index is to enable automated fraud detection, such as
-            validation for residency for accessing council services such as
-            school places, number of residents in a household for council tax
-            discount, or cases of illegal subletting of council housing.
+            of the index is to enable fraud detection, such as validation for
+            residency for accessing council services such as school places,
+            number of residents in a household for council tax discount, or
+            cases of illegal subletting of council housing.
           </p>
         </div>
       </div>
 
-      <div className="w-80 center  ">
-        <div className="fl w-30 tc ">
+      <div className="w-80 center flex">
+        <div className="fl w-30 mt3">
           <img
-            className="pr3 pt3 "
+            className="pr3 pt3"
             alt="Bristol Integrated Analytical Hub"
-            src="images/case_avon.png"
+            src="images/case_avon.jpg"
           />
         </div>
-        <div className="fl w-70 tl pb4">
+        <div className="fl w-70 tl pb4 ml3">
           <h2 className="pa3 f3 primary-color">
             Avon & Somerset Police Qlik Sense
           </h2>
@@ -113,17 +116,108 @@ const CaseStudies = () => (
             system is a form of self-service analytics software that connects
             internal datasets within Avon & Somerset Police, as well as some
             datasets from other agencies in Bristol Council, to provide
-            integrated assessments and evaluations. The focus here is the
-            predictive modeling for individual offenders and victims as well as
-            neighbourhood mapping of crime. Built into Qlik Sense applications
-            are offender risk scores and vulnerability risk scores along with a
-            harm rating that determines an overall risk. It is intended as a
-            ‘one-click’ system that provides individual offending and
-            intelligence profiles to help ‘triage’ risks and threats. The system
-            is used by frontline staff to decide on allocation of resources and
-            pathways of managing highest risk offenders. In some instances, such
-            as domestic abuse, it is used to decide on who to manage and to
-            enable pre-emptive measures.
+            integrated assessments and evaluations. The focus in this report is
+            the predictive modeling for individual offenders and victims as well
+            as neighbourhood mapping of crime. Built into Qlik Sense
+            applications are offender risk scores and vulnerability risk scores
+            along with a harm rating that determines an overall risk. It is
+            intended as a ‘one-click’ system that provides individual offending
+            and intelligence profiles to help ‘triage’ risks and threats. The
+            system is used by frontline staff to decide on allocation of
+            resources and pathways of managing highest risk offenders. In some
+            instances, such as domestic abuse, it is used to decide on who to
+            manage and to enable pre-emptive measures.
+          </p>
+        </div>
+      </div>
+
+      <div className="w-80 center flex">
+        <div className="fl w-30 mt3">
+          <img
+            className="pr3 pt3"
+            alt="Kent Integrated Dataset"
+            src="images/case_kent.jpg"
+          />
+        </div>
+        <div className="fl w-70 tl pb4 ml3">
+          <h2 className="pa3 f3 primary-color">Kent Integrated Dataset</h2>
+          <p className="pa3">
+            The Kent Integrated Dataset is used for population health planning.
+            It is not used to make decisions about specific individuals. Data
+            about individuals in the dataset are pseudonymised. Access to the
+            dataset is limited to the Kent Public Health Observatory team.
+            Controlled access is possible for research purposes but requires
+            application. In the spring of 2018 Kent County Council noted that
+            they have not deployed predictive analytics processes but that they
+            have hopes to in the future. They have done analyses that ‘project
+            into the future based on past trends’. They are developing a system
+            dynamics model, detailed below, to see the impact of health
+            prevention efforts on reducing smoking, obesity and others.The Kent
+            Clinical Commissioning Groups have contracted Optum, a healthcare
+            intelligence company, to provide their business analytics service.
+            Optum is going to create a new Kent Integrated Dataset to inform
+            commissioning decisions. Optum is part of the American UnitedHealth
+            Group. More research into the development of this new Optum
+            integrated dataset is ongoing.
+          </p>
+        </div>
+      </div>
+
+      <div className="w-80 center flex">
+        <div className="fl w-30 mt3">
+          <img
+            className="pr3 pt3"
+            alt="Hackney’s Early Help Profiling System"
+            src="images/case_hackney.jpg"
+          />
+        </div>
+        <div className="fl w-70 tl pb4 ml3">
+          <h2 className="pa3 f3 primary-color">
+            Hackney’s Early Help Profiling System
+          </h2>
+          <p className="pa3">
+            Hackney County Council makes use of data analytics in a number of
+            ways ranging from population level health analytics, fraud
+            detection, to child welfare. This summary focuses on uses of data
+            analytics in Hackney for child welfare. In child welfare, Hackney
+            County Council is working with Ernst & Young (EY) and Xantura on the
+            use of a system to identify children at risk of maltreatment and
+            families who need additional support. The system is called the Early
+            Help Profiling System. It has been funded by EY and London Councils.
+            The system is being trialed and alerts have already led to early
+            interventions.
+          </p>
+        </div>
+      </div>
+
+      <div className="w-80 center flex">
+        <div className="fl w-30 mt3">
+          <img
+            className="pr3 pt3"
+            alt="Manchester’s Research & Intelligence Database"
+            src="images/case_manchester.jpg"
+          />
+        </div>
+        <div className="fl w-70 tl pb4 ml3">
+          <h2 className="pa3 f3 primary-color">
+            Manchester’s Research & Intelligence Database
+          </h2>
+          <p className="pa3">
+            Manchester City Council is using a system it calls the Manchester
+            Research & Intelligence Database to identify Troubled Families,
+            families in need of support, to enable caseworkers to access more
+            information more quickly than previously and to enable the
+            collection and analysis of data to assess services and impact. The
+            Council developed the system by buying an IBM product called iBase.
+            The approach to using the system has been internally developed. It
+            is noted that the aim is to empower ‘lead professionals to make the
+            best use of data they are legally able to see, and is not about
+            replacing decision making or interpretation with system algorithms
+            or decisions’. The stated aim is to enable a more ‘holistic’
+            understanding of people, needs and services. Future goals include
+            developing decision making tools, building performance reporting
+            tools, building threshold and alerting tools and rolling the system
+            out to more users, as well as connecting more data.
           </p>
         </div>
       </div>
