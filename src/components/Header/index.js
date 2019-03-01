6bb7e93cc8f69dc67ca3c2699243d00ea5b9c2ef
Christo <christo@cryptodrunks.net>
2019-03-01 09:16:16 +0100

    feat: enabled overviews in the menu

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -14,11 +14,12 @@ const Header = ({location}: Props) => {
   const isDocumentsLocation = /^\/documents/.test(location.pathname);
   const isInsightsLocation = /^\/insights/.test(location.pathname);
   const isCaseStudiesLocation = /^\/case-studies/.test(location.pathname);
+  const isOverviewsLocation = /^\/overviews/.test(location.pathname);
   const isMethodologyLocation = /^\/methodology/.test(location.pathname);
 
   return (
-    <header className="cf ttu f6 ph2-ns flex-ns items-center">
-      <h1 className="f3 w-100 w-60-ns pa2">
+    <header className="cf ttu f6 ph2-ns flex-ns justify-between items-center">
+      <h1 className="f3 w-100 w-40-ns pa2">
         <Link className="link ma2 ttu white primary-color" to="/">
           <div className="logo">
             <img
@@ -29,7 +30,7 @@ const Header = ({location}: Props) => {
           </div>
         </Link>
       </h1>
-      <nav className="w-100 w-40-ns pa2-ns">
+      <nav className="w-100 w-60-ns pa2-ns right">
         <ul className="list">
           <li className="ma2-ns ma1 dib">
             <Link
@@ -59,6 +60,16 @@ const Header = ({location}: Props) => {
               }`}
             >
               Case Studies
+            </Link>
+          </li>
+          <li className="ma2-ns ma1 dib">
+            <Link
+              to="/overviews"
+              className={`link nowrap ma2 pb1 b black ${
+                isOverviewsLocation ? activeClass : "hover-nav-link"
+              }`}
+            >
+              Overviews
             </Link>
           </li>
           <li className="ma2-ns ma1 dib">
